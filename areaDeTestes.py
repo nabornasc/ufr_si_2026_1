6487d9cfaf11bc3f613ce60e2315197169fd026f
nabor do nascimento da silva <nabornasc@gmail.com>
2026-04-01 17:39:28 -0400

    v1 - Leitura de nome + calculo de data aniversario

--- areaDeTestes.py
+++ areaDeTestes.py
@@ -1,11 +1,25 @@
-def soma(a, b):
-    """Soma dois números inteiros"""
-    return a + b
+# codigo para digitar nome e salvar em uma variavel
+nome = str(input("Digite seu nome: "))
+print("Olá, " + nome + "! Bem-vindo ao Python!")
 
+# agora vai receber data de nascimento no formato ddmmaaaa, salvar em uma variavel e mostra minha idade em anos, meses e dias, considerando o ano atual.
+ano_nascimento = int(input("Digite seu nascimento (ddmmaaaa): "))
+# leia a data do pc para calcular a idade
+from datetime import datetime
+data_atual = datetime.now()
+ano_atual = data_atual.year
+mes_atual = data_atual.month
+dia_atual = data_atual.day
+# extrai o dia, mês e ano do nascimento
+dia_nascimento = ano_nascimento // 1000000
+print(dia_nascimento)
+mes_nascimento = (ano_nascimento // 10000) % 100
+print(mes_nascimento)
+ano_nascimento = ano_nascimento % 10000
+print(ano_nascimento)
+# calcula a idade em anos, meses e dias
+idade_anos = ano_atual - ano_nascimento
+idade_meses = mes_atual - mes_nascimento
+idade_dias = dia_atual - dia_nascimento
+print("Você tem " + str(idade_anos) + " anos, " + str(idade_meses) + " meses e " + str(idade_dias) + " dias.")
 
-# Exemplo de uso
-if __name__ == "__main__":
-    num1 = int(input("Digite o primeiro número: "))
-    num2 = int(input("Digite o segundo número: "))
-    resultado = soma(num1, num2)
-    print(f"A soma de {num1} + {num2} = {resultado}")
