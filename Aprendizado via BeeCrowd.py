983ffd5fb63b8788ec43ddb26582e4dbc748bf30
nabornasc <nabornasc@gmail.com>
2026-04-12 21:19:26 -0400

    Dia 2 de aprendizado BeeCrowd

--- Aprendizado via BeeCrowd.py
+++ Aprendizado via BeeCrowd.py
@@ -11,9 +11,91 @@
 
 # ==============================
 
-A=int(input())
-B=int(input())
+# A=int(input())
+# B=int(input())
 
-soma=A+B
+# soma=A+B
 
-print(f'SOMA = {soma}')
+# print(f'SOMA = {soma}')
+
+# ===============================
+
+
+# valor1=int(input())
+# valor2=int(input())
+
+# prod=valor1*valor2
+
+# print(f'PROD = {prod}')
+
+# =================================
+
+# a=float(input())*3.5
+# b=float(input())*7.5
+
+# media=(a+b)/11
+
+# print(f'MEDIA = {media:.5f}')
+
+# =================================
+
+# a=float(input())*2
+# b=float(input())*3
+# c=float(input())*5
+
+# media=(a+b+c)/10
+
+# print(f'MEDIA = {media:.1f}')
+
+# =================================
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+
+# diferenca=(a*b-c*d)
+
+# print(f'DIFERENCA = {diferenca}')
+
+# =================================
+
+# numFuncionario=int(input())
+# numHorasTrab=int(input())
+# valorPorHoras=float(input())
+
+# salario=numHorasTrab*valorPorHoras
+
+# print(f'NUMBER = {numFuncionario}')
+# print(f'SALARY = {salario:.2f}')
+
+# =================================
+
+# nomeVendedor=str(input())
+# salarioFixo=float(input())
+# totalVendas=float(input())*0.15
+
+# total=salarioFixo+totalVendas
+
+# print(f'TOTAL = R$ {total:.2f}')
+
+# =================================
+
+codigoPeca1, numPeca1, valorPeca1 = input().split()
+codigoPeca2, numPeca2, valorPeca2 = input().split()
+
+fatura = int(numPeca1) * float(valorPeca1) + int(numPeca2) * float(valorPeca2)
+
+print(f'VALOR A PAGAR: R$ {fatura:.2f}')
+
+# codigoPeca1=int(input())
+# numPeca1=int(input())
+# valorPeca1=float(input())
+
+# codigoPeca2=int(input())
+# numPeca2=int(input())
+# valorPeca2=float(input())
+
+# fatura=(numPeca1*valorPeca1)+(numPeca2*valorPeca2)
+
+# print(f'VALOR A PAGAR: R$ {fatura:.2f}')
