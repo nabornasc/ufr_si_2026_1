4152e9bfcbed611dd90a9f39b340e99b967c5d7a
NaborNasc <39881126+nabornasc@users.noreply.github.com>
2026-05-07 22:11:17 -0400

    BeeCrowd 1050

--- Aprendizado via BeeCrowd.py
+++ Aprendizado via BeeCrowd.py
@@ -455,3 +455,18 @@
 #             print(list4[6])
 #         else:
 #             print(list4[7])
+
+ddd=int(input())
+
+cidades={
+    61: 'Brasilia',
+    71: "Salvador",
+    11: "Sao Paulo",
+    21: "Rio de Janeiro",
+    32: "Juiz de Fora",
+    19: "Campinas",
+    27: "Vitoria",
+    31: "Belo Horizonte"
+}
+
+print(cidades.get(ddd,'DDD não cadastrado'))
