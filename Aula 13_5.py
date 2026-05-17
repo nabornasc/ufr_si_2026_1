2903a2a08dd4440bd23312c37d9bbea13e11d88c
NaborNasc <39881126+nabornasc@users.noreply.github.com>
2026-05-17 09:39:51 -0400

    Loop For E01

--- Aula 13_5.py
+++ Aula 13_5.py
@@ -1,8 +1,12 @@
+a = int(input('Quer iniciar em [0/1]: '))
+b = int(input('Quer ir até [10,100,..: '))
+c = int(input('Quer contar de quanto em quanto [1,2,..]: '))
+d = str(input('Crescente ou decrescente [C/D]: ').lower())
 
-a=int(input('Quer iniciar em [0/1]: '))
-b=int(input('Quer ir até [10,100,..: '))
-c=int(input('Quer contar de quanto em quanto [1,2,..]: '))
-
-for i in range (a,b+1,c):
-    print(i)
-    i+=i
+for i in range(a, b + 1, c):
+    if d == 'c':
+        print(i)
+        i += i
+    elif d == 'd':
+        print(b)
+        b -= c
