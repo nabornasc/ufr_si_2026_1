294e8271dfd6b90e597b0c04a3701b9180d87c6c
nabor do nascimento da silva <nabornasc@gmail.com>
2026-05-20 17:28:48 -0400

    Treino 20/5 .1

--- Aula 13_5.py
+++ Aula 13_5.py
@@ -24,16 +24,65 @@
 # print(resul)
 
 # ============================================================
-print('Programa para somar "P.As"')
-pa=int(input('Deseja uma PA "Progressão Aritmética de quantos números: '))
-rz=int(input('Qual a razão dessa PA "1/1; 2/2; 3/3.. : '))
-soma=0
-print('Passo a passo |   |   |')
-for cont in range(1,pa+1):
-    if rz==1:
-        print('Passo a passo ',soma,'+',cont)
-        soma+=cont
+#
+# print('Programa para somar "P.As"')
+# pa=int(input('Deseja uma PA "Progressão Aritmética de quantos números: '))
+# rz=int(input('Qual a razão dessa PA "1/1; 2/2; 3/3.. : '))
+# soma=0
+# print('Passo a passo |   |   |')
+# for cont in range(1,pa+1):
+#     if rz==1:
+#         print('Passo a passo ',soma,'+',cont)
+#         soma+=cont
+#     else:
+#         print('Passo a passo ',soma,'+',cont*rz)
+#         soma+=cont*rz
+# print(soma)
+
+# ============================================================
+from os import system,name
+
+def limpar():
+    if name == 'nt':
+        system('cls')
     else:
-        print('Passo a passo ',soma,'+',cont*rz)
-        soma+=cont*rz
-print(soma)
+        system('clear')
+
+def progA():
+    print('Programa para somar "P.As"')
+    pa=int(input('Deseja uma PA "Progressão Aritmética de quantos números: '))
+    rz=int(input('Qual a razão dessa PA "1/1; 2/2; 3/3.. : '))
+    soma=0
+    print('Passo a passo |   |   |')
+    for cont in range(1,pa+1):
+        if rz==1:
+            print('Passo a passo ',soma,'+',cont)
+            soma+=cont
+        else:
+            print('Passo a passo ',soma,'+',cont*rz)
+            soma+=cont*rz
+    print('Soma final: ',soma)
+
+def fatorial(n):
+    mult = 1
+    for cont in range(1,n+1,1):
+        mult = mult * cont
+        if cont < n:
+            print(cont,end="*")
+        else:
+            print(cont,end=" ")
+            print(f'= {mult:,}')
+    return mult
+
+print('Escolha a Função!')
+print('[1] PA "Progressão Aritimetica"')
+print('[2] Fatorial')
+opc=int(input())
+limpar()
+
+if opc == 1:
+    limpar()
+    progA()
+elif opc == 2:
+    limpar()
+    fatorial(int(input('Digite n!: ')))
