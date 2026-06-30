ad486b879270a9b8948b24313b5fdf91748661a6
nabor do nascimento da silva <nabornasc@gmail.com>
2026-06-30 14:04:47 -0400

    Edit---None

--- areaDeTestes_1.py
+++ areaDeTestes_1.py
@@ -4,3 +4,7 @@ cls = lambda: os.system("cls" if os.name == "nt" else "clear")
 cls()
 
 print("Oi Mundo")
+
+matA = [10, 25, 32, 71, 57, 13, 29]
+for cont in range(len(matA)):
+    print(matA[cont])
